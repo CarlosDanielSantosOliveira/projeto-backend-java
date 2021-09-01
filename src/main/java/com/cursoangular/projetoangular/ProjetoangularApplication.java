33db726cae0d9d20cec77ff1391b50ff9138d18c
Carlos Daniel <carlos_daniel_921@outlook.com>
2021-09-01 04:47:27 -0300

    Projeto Criado

---
+++ src/main/java/com/cursoangular/projetoangular/ProjetoangularApplication.java
@@ -0,0 +1,13 @@
+package com.cursoangular.projetoangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoangularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoangularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursoangular/projetoangular/ProjetoangularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursoangular.projetoangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoangularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
